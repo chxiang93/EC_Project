7ab3d9244d69caec48c042bc6ec7b554e120b53a
Chen Xiang <scxxiang@gmail.com>
2022-06-02 15:06:51 +0800

    change_from_wdbc_to_wpbc

--- FeatureSelectionUsingGA.cpp
+++ FeatureSelectionUsingGA.cpp
@@ -5,12 +5,17 @@
 #include <Python.h>
 using namespace std;
 
-const int GENE = 30;
-const int POP_SIZE = 30;
-const string ATTRIBUTE[GENE] = { "radius(mean)", "texture(mean)", "perimeter(mean)", "area(mean)", "smoothness(mean)", "compactness(mean)", "cancavity(mean)", "concave points(mean)","symmetry(mean)","fractal dimension(mean)",    
-                                 "radius(standard error)", "texture(standard error)", "perimeter(standard error)", "area(standard error)", "smoothness(standard error)", "compactness(standard error)", "cancavity(standard error)", 
-                                 "concave points(standard error)","symmetry(standard error)","fractal dimension(standard error)", "radius(worst)", "texture(worst)", "perimeter(worst)", "area(worst)", "smoothness(worst)", 
-                                 "compactness(worst)", "cancavity(worst)", "concave points(worst)","symmetry(worst)","fractal dimension(worst)" };
+const int GENE = 33;
+const int POP_SIZE = 500;
+//const string ATTRIBUTE[GENE] = { "radius(mean)", "texture(mean)", "perimeter(mean)", "area(mean)", "smoothness(mean)", "compactness(mean)", "cancavity(mean)", "concave points(mean)","symmetry(mean)","fractal dimension(mean)",    
+//                                 "radius(standard error)", "texture(standard error)", "perimeter(standard error)", "area(standard error)", "smoothness(standard error)", "compactness(standard error)", "cancavity(standard error)", 
+//                                 "concave points(standard error)","symmetry(standard error)","fractal dimension(standard error)", "radius(worst)", "texture(worst)", "perimeter(worst)", "area(worst)", "smoothness(worst)", 
+//                                 "compactness(worst)", "cancavity(worst)", "concave points(worst)","symmetry(worst)","fractal dimension(worst)" };
+
+const string ATTRIBUTE[GENE] = { "time", "radius(mean)", "texture(mean)", "perimeter(mean)", "area(mean)", "smoothness(mean)", "compactness(mean)", "cancavity(mean)", "concave points(mean)","symmetry(mean)","fractal dimension(mean)",
+                                 "radius(standard error)", "texture(standard error)", "perimeter(standard error)", "area(standard error)", "smoothness(standard error)", "compactness(standard error)", "cancavity(standard error)",
+                                 "concave points(standard error)","symmetry(standard error)","fractal dimension(standard error)", "radius(worst)", "texture(worst)", "perimeter(worst)", "area(worst)", "smoothness(worst)",
+                                 "compactness(worst)", "cancavity(worst)", "concave points(worst)","symmetry(worst)","fractal dimension(worst)", "tumor size", "lymph node status" };
 const double CROSSOVER_PROBABILITY = 0.9;
 const double MUTATION_PROBABILITY = 0.2;
 const int MAXIMUM_GENERATION = 30;
@@ -25,10 +30,17 @@ void initialisePopulation()
 
     for (int c = 0; c < POP_SIZE; c++)
     {
-        for (int g = 0; g < GENE; g++)
-        {
-            chromosome[c][g] = rand() % 2;
-        }
+        int sum = 0;
+
+        //do {
+            sum = 0;
+
+            for (int g = 0; g < GENE; g++)
+            {
+                chromosome[c][g] = rand() % 2;
+                sum += chromosome[c][g];
+            }
+        //} while (sum > 2);
     }
 }
 
@@ -64,6 +76,7 @@ void evaluateChromosome(int argc, char* argv[])
         "import pandas as pd\n"
         "from sklearn.model_selection import train_test_split\n"
         "from sklearn.neighbors import KNeighborsClassifier\n"
+        "from sklearn.ensemble import RandomForestClassifier\n"
         "from sklearn.metrics import confusion_matrix\n"
         "from sklearn.metrics import roc_curve\n"
         "from sklearn.metrics import roc_auc_score\n"
@@ -74,10 +87,15 @@ void evaluateChromosome(int argc, char* argv[])
     for (int i = 0; i < POP_SIZE; i++)
     {
         PyRun_SimpleString(
-            "df = pd.read_csv('wdbc.csv')\n"
-            "df.columns = ['ID number', 'Class', 'radius(mean)', 'texture(mean)', 'perimeter(mean)', 'area(mean)', 'smoothness(mean)', 'compactness(mean)', 'cancavity(mean)', 'concave points(mean)', 'symmetry(mean)', 'fractal dimension(mean)', \\"
+            //"df = pd.read_csv('wdbc.csv')\n"
+            "df = pd.read_csv('wpbc.csv')\n"
+            //"df.columns = ['ID number', 'Class', 'radius(mean)', 'texture(mean)', 'perimeter(mean)', 'area(mean)', 'smoothness(mean)', 'compactness(mean)', 'cancavity(mean)', 'concave points(mean)', 'symmetry(mean)', 'fractal dimension(mean)', \\"
+           // "\n'radius(standard error)', 'texture(standard error)', 'perimeter(standard error)', 'area(standard error)', 'smoothness(standard error)', 'compactness(standard error)', 'cancavity(standard error)', 'concave points(standard error)', 'symmetry(standard error)', 'fractal dimension(standard error)', \\"
+           // "\n'radius(worst)', 'texture(worst)', 'perimeter(worst)', 'area(worst)', 'smoothness(worst)', 'compactness(worst)', 'cancavity(worst)', 'concave points(worst)', 'symmetry(worst)', 'fractal dimension(worst)']\n"
+            
+            "df.columns = ['ID number', 'Class', 'time', 'radius(mean)', 'texture(mean)', 'perimeter(mean)', 'area(mean)', 'smoothness(mean)', 'compactness(mean)', 'cancavity(mean)', 'concave points(mean)', 'symmetry(mean)', 'fractal dimension(mean)', \\"
             "\n'radius(standard error)', 'texture(standard error)', 'perimeter(standard error)', 'area(standard error)', 'smoothness(standard error)', 'compactness(standard error)', 'cancavity(standard error)', 'concave points(standard error)', 'symmetry(standard error)', 'fractal dimension(standard error)', \\"
-            "\n'radius(worst)', 'texture(worst)', 'perimeter(worst)', 'area(worst)', 'smoothness(worst)', 'compactness(worst)', 'cancavity(worst)', 'concave points(worst)', 'symmetry(worst)', 'fractal dimension(worst)']\n"
+            "\n'radius(worst)', 'texture(worst)', 'perimeter(worst)', 'area(worst)', 'smoothness(worst)', 'compactness(worst)', 'cancavity(worst)', 'concave points(worst)', 'symmetry(worst)', 'fractal dimension(worst)', 'tumor size', 'lymph node status']\n"
             "df = df.drop(columns = 'ID number')\n"
             "df = df.replace('?', np.NaN)\n"
             "df = df.dropna()\n"
@@ -91,9 +109,9 @@ void evaluateChromosome(int argc, char* argv[])
             "X = df[features].values\n"
             "y = df['Class'].values\n"
             "for c in range(len(y)):\n"
-            "\tif y[c] == 'B' :\n"
+            "\tif y[c] == 'R' :\n"
             "\t\ty[c] = 0\n"
-            "\telif y[c] == 'M' :\n"
+            "\telif y[c] == 'N' :\n"
             "\t\ty[c] = 1\n"
             "y = y.astype('int')\n"
             "X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42, stratify = y)\n"
@@ -105,9 +123,13 @@ void evaluateChromosome(int argc, char* argv[])
             "\tknn.fit(X_train, y_train)\n"
             "\ttrain_accuracy[i] = knn.score(X_train, y_train)\n"
             "\ttest_accuracy[i] = knn.score(X_test, y_test)\n"
-            "knn = KNeighborsClassifier(n_neighbors = 3)\n"
-            "knn.fit(X_train, y_train)\n"
-            "accuracy = knn.score(X_test, y_test)\n"
+            //"knn = KNeighborsClassifier(n_neighbors = 10)\n"
+            //"knn.fit(X_train, y_train)\n"
+            //"accuracy = knn.score(X_test, y_test)\n"
+            "model = RandomForestClassifier(n_estimators = 100, random_state = 0, n_jobs = -1)\n"
+            "model.fit(X_train, y_train)\n"
+            "y_pred = model.predict(X_test)\n"
+            "accuracy = model.score(X_test, y_test)\n"
             "with open('accuracy_result.txt', 'a') as f :\n"
             "\tf.write(f'{accuracy}\\n')\n"
         );
--- KNN.py
+++ KNN.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve
@@ -12,25 +13,29 @@ accuracyFile = open('accuracy_result.txt','w')
 accuracyFile.close()
 
 # Load the dataset
-df = pd.read_csv('wdbc.csv')
-df.columns = ['ID number', 'Class', 'radius(mean)', 'texture(mean)', 'perimeter(mean)', 'area(mean)', 'smoothness(mean)', 'compactness(mean)', 'cancavity(mean)', 'concave points(mean)','symmetry(mean)','fractal dimension(mean)',    \
+df = pd.read_csv('wpbc.csv')
+#df.columns = ['ID number', 'Class', 'radius(mean)', 'texture(mean)', 'perimeter(mean)', 'area(mean)', 'smoothness(mean)', 'compactness(mean)', 'cancavity(mean)', 'concave points(mean)','symmetry(mean)','fractal dimension(mean)',    \
+#'radius(standard error)', 'texture(standard error)', 'perimeter(standard error)', 'area(standard error)', 'smoothness(standard error)', 'compactness(standard error)', 'cancavity(standard error)', 'concave points(standard error)','symmetry(standard error)','fractal dimension(standard error)', \
+#'radius(worst)', 'texture(worst)', 'perimeter(worst)', 'area(worst)', 'smoothness(worst)', 'compactness(worst)', 'cancavity(worst)', 'concave points(worst)','symmetry(worst)','fractal dimension(worst)']
+
+df.columns = ['ID number', 'Class', 'time','radius(mean)', 'texture(mean)', 'perimeter(mean)', 'area(mean)', 'smoothness(mean)', 'compactness(mean)', 'cancavity(mean)', 'concave points(mean)','symmetry(mean)','fractal dimension(mean)',    \
 'radius(standard error)', 'texture(standard error)', 'perimeter(standard error)', 'area(standard error)', 'smoothness(standard error)', 'compactness(standard error)', 'cancavity(standard error)', 'concave points(standard error)','symmetry(standard error)','fractal dimension(standard error)', \
-'radius(worst)', 'texture(worst)', 'perimeter(worst)', 'area(worst)', 'smoothness(worst)', 'compactness(worst)', 'cancavity(worst)', 'concave points(worst)','symmetry(worst)','fractal dimension(worst)']
+'radius(worst)', 'texture(worst)', 'perimeter(worst)', 'area(worst)', 'smoothness(worst)', 'compactness(worst)', 'cancavity(worst)', 'concave points(worst)','symmetry(worst)','fractal dimension(worst)','tumor size','lymph node status']
 df = df.drop(columns= 'ID number')
 df = df.replace('?', np.NaN)
 df = df.dropna()
 
 # Let's create numpy arrays for features and target
 #X = df.drop('Class',axis=1).values
-features=['smoothness(mean)','texture(standard error)',]
+features=['radius(mean)', 'texture(mean)','time']
 X = df[features].values
 y = df['Class'].values
 
 # Change the values of y into 0 and 1 only
 for c in range(len(y)):
-    if y[c] == 'B':
+    if y[c] == 'R':
         y[c] = 0
-    elif y[c] == 'M':
+    elif y[c] == 'N':
         y[c] = 1
 
 y = y.astype('int')
@@ -66,10 +71,14 @@ knn = KNeighborsClassifier(n_neighbors=6)
 # Fit the model
 knn.fit(X_train,y_train)
 
-knn.score(X_test,y_test)
+#knn.score(X_test,y_test)
 
 # Let us get the predictions using the classifier we had fit above
-y_pred = knn.predict(X_test)
+#y_pred = knn.predict(X_test)
+
+model = RandomForestClassifier(n_estimators = 100, random_state = 0, n_jobs = -1)
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
 
 # Confusion Matrix
 cm = confusion_matrix(y_test,y_pred)
@@ -89,6 +98,7 @@ print('Specificity :', TN/(TN+FP))
 print('Precision \t:', TP/(TP+FP))
 print('Recall \t\t:', TP/(TP+FN))
 print('Accuracy \t:', accuracy)
+print(model.score(X_test,y_test))
 print(knn.score(X_test,y_test))
 
 with open("accuracy_result.txt","a") as f:
