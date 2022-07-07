6b9acc8f512d173159ecbcd8fbbb5bf5296f4330
Chen Xiang <scxxiang@gmail.com>
2022-07-07 12:49:10 +0800

    Final

--- FeatureSelectionUsingGA.cpp
+++ FeatureSelectionUsingGA.cpp
@@ -1,93 +1,82 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <vector>
+#include <stdlib.h>
+#include <ctime>
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 using namespace std;
 
+// Declaration of hyperparameter of the genetic algorithm
 const int GENE = 34;
-const int POP_SIZE = 30;
 const string ATTRIBUTE[GENE] = { "erythema", "scaling", "definite_borders","itching","koebner_phenomenon","polygonal_papules","follicular_papules","oral_mucosal_involvement","knee_and_elbow_involvement","scalp_involvement",
                                  "family_history","melanin_incontinence","eosinophils_in_the_infiltrate","PNL_infiltrate","fibrosis_of_the_papillary_dermis","exocytosis","acanthosis","hyperkeratosis","parakeratosis","clubbing_of_the_rete_ridges",
                                  "elongation_of_the_rete_ridges","thinning_of_the_suprapapillary_epidermis","spongiform_pustule","munro_microabcess","focal_hypergranulosis","disappearance_of_the_granular_layer","vacuolisation_and_damage_of_basal_layer","spongiosis","saw-tooth_appearance_of_rete","follicular_horn_plug",
                                  "perifollicular_parakeratosis","inflammatory_monoluclear_inflitrate","band-like_infiltrate","age"};
-const double CROSSOVER_PROBABILITY = 0.8;
+const int POP_SIZE = 50;
+const double CROSSOVER_PROBABILITY = 0.9;
 const double MUTATION_PROBABILITY = 0.2;
-const int MAXIMUM_GENERATION = 50;
-
-//int chromosome[POP_SIZE][GENE];
-//int sumChromosome[POP_SIZE];
-//double fitnessValue[POP_SIZE];
-//int parents[2][GENE];
-//int children[2][GENE];
-//int survivor[POP_SIZE][GENE];
-//int counterSurvival{ 0 };
-//double averageFitness = 0;
-//double bestFitness = -1;
-//int bestChromosome[GENE];
+const int MAXIMUM_GENERATION = 30;
 ofstream ACP_File("ACP_File.txt");
 ofstream BSF_File("BSF_File.txt");
 ofstream bestChromosome_File("bestChromosome_File.txt");
 
+// Declaration of class GA which consist of the whole process of the genetic algorithm
 class GA
 {
 public:
-    GA(const char* path);
-    ~GA();
-    void printChromosome();
-    void initialisePopulation();
-    void evaluateChromosome();
-    void parentSelection();
-    void crossover();
-    void mutation();
-    void survivalSelection();
-    void copyChromosome();
-    void calculateAverageFitness();
-    void recordBestFitness();
-    int counterSurvival{ 0 };
+    GA(const char* path);   // Constructor that accept a path parameter to initialize python environment
+    ~GA();                  // Destructor
+    void printChromosome(); // Print all the chromosome in the population
+    void initialisePopulation();    // Initialise the population of chromosome randomly
+    void evaluateChromosome();      // Evaluate the fitness of every chromosome 
+    void parentSelection();         // Tournament selection with 3 players to select parents
+    void crossover();               // 2-point crossover to produce children
+    void mutation();                // Bit-flip mutation on random 2 gene in a chromosome
+    void survivalSelection();       // All children replace parents strategy
+    void copyChromosome();          // Copy all the survivor to the population of chromosome for next generation
+    void calculateAverageFitness(); // Calculate the average fitness (ACP)
+    void recordBestFitness();       // Record BSF and best chromosome
+
+    int counterSurvival{ 0 };       // Counter that keep track of number of surivor
     
 private:
-    int chromosome[POP_SIZE][GENE];
-    int sumChromosome[POP_SIZE];
-    double fitnessValue[POP_SIZE];
-    int parents[2][GENE];
-    int children[2][GENE];
-    int survivor[POP_SIZE][GENE];
-    double averageFitness = 0;
-    double bestFitness = -1;
-    int bestChromosome[GENE];
+    int chromosome[POP_SIZE][GENE]; // Chromosome
+    int sumAttribute[POP_SIZE];     // Number of attribute selected for every chromosome
+    double fitnessValue[POP_SIZE];  // Fitness value of every chromosome
+    int parents[2][GENE];           // Parents
+    int children[2][GENE];          // Children
+    int survivor[POP_SIZE][GENE];   // Survivor
+    double averageFitness = 0;      // ACP
+    double bestFitness = -1;        // BSF
+    int bestChromosome[GENE];       // Best chromosome
 
-    wchar_t* program;
-    void initializePython(const char* path);
-    void closePython();
+    wchar_t* program;               // Python program
+    void initializePython(const char* path);    // Function to initialize python environment
+    void closePython();             // Function to clean up python
 };
 
+// Main function
 int main(int argc, char* argv[])
 {
+    // Path that contain the solution file
     const char* path = argv[0];
 
+    // Instantiate a GA object for featureSelection
     GA featureSelectionGA(path);
-   /* cout << "\nINITIALIZE POPULATION\n";
-    initialisePopulation();
-    printChromosome();
-
-    cout << "\nFITNESS EVALUATION\n";
-    evaluateChromosome(path);
-
-    cout << "\n\nPARENT SELECTION";
-    parentSelection();*/
-
+   
+    // Initialize population
     cout << "\nINITIALIZE POPULATION\n";
     featureSelectionGA.initialisePopulation();
 
+    // Iteration until MAXIMUM_GENERATION
     for (int j = 0; j < MAXIMUM_GENERATION; j++)
     {
-        cout << "\n***************************GENERATION " << j + 1 << "****************************";
+        cout << "\n********************************GENERATION " << j + 1 << "*************************************";
         cout << "\n\nNEW POPULATION\n";
         featureSelectionGA.printChromosome();
         //getchar();
-
+        
         featureSelectionGA.counterSurvival = 0;
 
         cout << "\nFITNESS EVALUATION";
@@ -150,19 +139,17 @@ void GA::initialisePopulation()
 
     for (int c = 0; c < POP_SIZE; c++)
     {
-        int sum = 0;
+        int sum;
 
-       // do {
-            sum = 0;
+		sum = 0;
 
-            for (int g = 0; g < GENE; g++)
-            {
-                chromosome[c][g] = rand() % 2;
-                sum += chromosome[c][g];
-            }
+		for (int g = 0; g < GENE; g++)
+		{
+			chromosome[c][g] = rand() % 2;
+			sum += chromosome[c][g];
+		}
 
-            sumChromosome[c] = sum;
-       // } while (sum > 10);
+		sumAttribute[c] = sum;
     }
 }
 
@@ -230,19 +217,20 @@ void GA::evaluateChromosome()
         );
     }
 
-    ifstream fitnessFile("accuracy_result.txt");
+    ifstream accuracyFile("accuracy_result.txt");
 
-    for (int i = 0; fitnessFile; i++)
+    for (int i = 0; i < POP_SIZE; i++)
     {
         double accuracy;
-        //fitnessFile >> fitnessValue[i];
-        fitnessFile >> accuracy;
+        
+        accuracyFile >> accuracy;
 
-        //fitnessValue[i] = (accuracy + (1 / (sumChromosome[i] + 0.01))) / 2.0;
-        fitnessValue[i] = accuracy * ((static_cast<double>(GENE) - sumChromosome[i]) / (GENE));
+        fitnessValue[i] = (accuracy * 0.7) + (((static_cast<double>(GENE) - sumAttribute[i]) / (GENE)) * 0.3);
+        
+        //fitnessValue[i] = accuracy * ((static_cast<double>(GENE) - sumAttribute[i]) / (GENE));
     }
 
-    fitnessFile.close();
+    accuracyFile.close();
 
     for (int c = 0; c < POP_SIZE; c++)
     {
@@ -252,37 +240,57 @@ void GA::evaluateChromosome()
 
 void GA::parentSelection()
 {
-    // tournament selection 2 player
+    // tournament selection 3 player
 
-    int indexplayer1, indexplayer2;
-    int winnerIndex[2];
+    int indexplayer1, indexplayer2, indexplayer3;
+    int winnerIndex[2] = {};
 
     do
     {
-        for (int p = 0; p < 2; p++)	// 2 players
+        for (int p = 0; p < 2; p++)	// 2 parents
         {
             indexplayer1 = rand() % POP_SIZE;
 
             do
             {
                 indexplayer2 = rand() % POP_SIZE;
+                indexplayer3 = rand() % POP_SIZE;
 
-            } while (indexplayer1 == indexplayer2);
+            } while ((indexplayer1 == indexplayer2) || (indexplayer1 == indexplayer3) || (indexplayer2 == indexplayer3));
 
-            cout << "\n\tround " << p + 1 << ":";
-            cout << "\n\t player 1 :" << indexplayer1;
-            cout << "\n\t player 2 :" << indexplayer2;
+            cout << "\n\tRound " << p + 1 << ":";
+            cout << "\n\t Player 1 :" << indexplayer1;
+            cout << "\n\t Player 2 :" << indexplayer2;
+            cout << "\n\t Player 3 :" << indexplayer3;
 
-            if (fitnessValue[indexplayer1] > fitnessValue[indexplayer2])
-            {
-                winnerIndex[p] = indexplayer1;
+            if (fitnessValue[indexplayer1] >= fitnessValue[indexplayer2]) {		// Check if fitness value for player1 is greater than player2
+
+                if (fitnessValue[indexplayer1] >= fitnessValue[indexplayer3])	// If yes, then compare player 1 with player3. Player2 does not need to compare
+                {											// because it is lesser than player1
+                    winnerIndex[p] = indexplayer1;
+                }
+                else	  // If player1 lesser than player3, the player3 will be the winner
+                {
+                    winnerIndex[p] = indexplayer3;
+                }
             }
-            else
-            {
-                winnerIndex[p] = indexplayer2;
+            else if (fitnessValue[indexplayer1] < fitnessValue[indexplayer2]) { // Check if fitness value for player1 is lesser than player2
+
+                if (fitnessValue[indexplayer2] >= fitnessValue[indexplayer3])  // If yes, then compare player 2 with player3. Player1 does not need to compare
+                {										   // because it is lesser than player2
+                    winnerIndex[p] = indexplayer2;
+                }
+                else   // If player2 lesser than player3, the player3 will be the winner
+                {
+                    winnerIndex[p] = indexplayer3;
+                }
             }
 
-            cout << "\nwinner :" << winnerIndex[p];
+            cout << "\n\tPlayers: " << indexplayer1 << " vs " << indexplayer2 << " vs " << indexplayer3;
+            cout << "\tFitness: " << fitnessValue[indexplayer1] << " vs " << fitnessValue[indexplayer2] << " vs " << fitnessValue[indexplayer3];
+            cout << "\n\tWinner - Parents: " << winnerIndex[p] << "\tFitness: " << fitnessValue[winnerIndex[p]];
+            cout << endl;
+
             for (int g = 0; g < GENE; g++)
             {
                 parents[p][g] = chromosome[winnerIndex[p]][g];
@@ -294,7 +302,7 @@ void GA::parentSelection()
     cout << "\n\nResult:";
     for (int p = 0; p < 2; p++)
     {
-        cout << "\n\tparent " << p << ": ";
+        cout << "\n\tParent " << p + 1 << ": ";
 
         for (int g = 0; g < GENE; g++)
         {
@@ -305,8 +313,10 @@ void GA::parentSelection()
 
 void GA::crossover()
 {
+    // 2 point crossover
     float randomValue;
-    int crossoverPoint;
+    int crossover_point;
+    int crossover_point2;
 
     for (int r = 0; r < 2; r++)
     {
@@ -316,30 +326,43 @@ void GA::crossover()
         }
     }
 
-    randomValue = (rand() % 10) / 10.0;
+    randomValue = (rand() % 34) / 34.0;
     cout << "\n\tRandomValue = " << randomValue;
 
     if (randomValue < CROSSOVER_PROBABILITY)
     {
-        srand(time(0));
-        crossoverPoint = rand() % GENE;
+        //srand(time(0));
+        crossover_point = rand() % GENE;
 
-        cout << "\n\tCrossover Point = " << crossoverPoint;
-
-        for (int g = crossoverPoint; g < GENE; g++)
+        do
         {
-            children[0][g] = parents[1][g];
-            children[1][g] = parents[0][g];
+            crossover_point2 = rand() % GENE;
+
+        } while (crossover_point == crossover_point2);
+
+        if (crossover_point > crossover_point2) {
+            int tmp = crossover_point;
+            crossover_point = crossover_point2;
+            crossover_point2 = tmp;
+        }
+
+        cout << "\n\tCrossover Point 1 = " << crossover_point;
+        cout << "\n\tCrossover Point 2 = " << crossover_point2;
+
+        for (int c = crossover_point; c <= crossover_point2; c++)
+        {
+            children[0][c] = parents[1][c];
+            children[1][c] = parents[0][c];
         }
 
         cout << "\nResult of crossover:";
-        for (int p = 0; p < 2; p++)
+        for (int c = 0; c < 2; c++)
         {
-            cout << "\nChildren " << p << ": ";
+            cout << "\nChildren " << c + 1 << ": ";
 
             for (int g = 0; g < GENE; g++)
             {
-                cout << children[p][g] << " ";
+                cout << children[c][g] << " ";
             }
         }
     }
@@ -347,50 +370,69 @@ void GA::crossover()
 
 void GA::mutation()
 {
+    // bit flip mutation (2 genes per chromosome)
     float randomValue;
     int mutationBit;
+    int mutationBit2;
 
     for (int c = 0; c < 2; c++)
     {
-        randomValue = (rand() % 10) / 10.0;
+        randomValue = (rand() % 34 / 34.0);
 
-        cout << "\n  Children " << c << ":";
+        cout << "\nChildren " << c + 1 << ":";
         cout << "\n\tRandomValue = " << randomValue;
 
         if (randomValue < MUTATION_PROBABILITY)
         {
             mutationBit = rand() % GENE;
 
-            cout << "\n\tMutation Bit = " << mutationBit;
-
-            if (children[c][mutationBit] == 0)
+            do
             {
-                children[c][mutationBit] = 1;
-            }
-            else
+                mutationBit2 = rand() % GENE;
+
+            } while (mutationBit == mutationBit2);
+
+            cout << "\n\tMutation Bit 1 = " << mutationBit;
+            cout << "\n\tMutation Bit 2 = " << mutationBit2;
+
+            if (children[c][mutationBit] == 1)
             {
                 children[c][mutationBit] = 0;
             }
-        }
-    }
+            else
+            {
+                children[c][mutationBit] = 1;
+            }
+
+            if (children[c][mutationBit2] == 1)
+            {
+                children[c][mutationBit2] = 0;
+            }
+            else
+            {
+                children[c][mutationBit2] = 1;
+            }//if check bit
+        }//if random value
+    }//for 2 children
 
     cout << "\nResult of mutation:";
-    for (int p = 0; p < 2; p++)
+    for (int c = 0; c < 2; c++)
     {
-        cout << "\nChildren " << p << ": ";
+        cout << "\n\tChildren " << c + 1 << ": ";
 
         for (int g = 0; g < GENE; g++)
         {
-            cout << children[p][g] << " ";
+            cout << children[c][g] << " ";
         }
     }
 }
 
 void GA::survivalSelection()
 {
+    // All children replace parents
     for (int c = 0; c < 2; c++)
     {
-        for (int g = 0; g < GENE; g++)
+        for (int g = 0; g < GENE; g++)//copy survivor from children
         {
             survivor[counterSurvival][g] = children[c][g];
         }
@@ -400,9 +442,10 @@ void GA::survivalSelection()
 
     cout << "\nSurvivor: \n";
 
+    //2. Update array counter
     for (int c = 0; c < POP_SIZE; c++)
     {
-        cout << "survivor " << c << " : ";
+        cout << "\tSurvivor " << c + 1 << " : ";
 
         for (int g = 0; g < GENE; g++)
         {
